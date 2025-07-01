b25b9b8400ad6cf0cb3200ea52303019335e5487
Nikita Gureenkov <gureenkov.n@promail366.com>
2025-07-01 23:16:39 +0200

    Some fixes

--- server/middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler((event) => {
-    const accessKey = getCookie(event, 'access_key')
-    const secret = useRuntimeConfig().public.secretKey
-
-    if (accessKey !== secret) {
-        throw createError({ statusCode: 401, message: 'Unauthorized' })
-    }
-})
